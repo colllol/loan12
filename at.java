fce29bdb6e80e26942df03b92e6280f20b32dcaa
[BOT] Nosense <uchihanosense@gmail.com>
2024-11-08 22:33:41 +0700

    init

---
+++ at.java
@@ -0,0 +1,9 @@
+public interface at {
+  void c();
+}
+
+
+/* Location:              C:\Users\bot-nosense\Downloads\Loan-12-Su-Quan.jar!\at.class
+ * Java compiler version: 1 (45.3)
+ * JD-Core Version:       1.1.3
+ */
---
+++ ba.java
@@ -0,0 +1,9 @@
+interface ba {
+  void a(String paramString1, String paramString2);
+}
+
+
+/* Location:              C:\Users\bot-nosense\Downloads\Loan-12-Su-Quan.jar!\ba.class
+ * Java compiler version: 1 (45.3)
+ * JD-Core Version:       1.1.3
+ */
